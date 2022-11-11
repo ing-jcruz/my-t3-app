af8a55594bbce80c099507f8e28f3cbb057d9680
Jesus Cruz <jcruz@sumimsa.com.mx>
2022-11-11 12:48:04 -0600

    se agregan estilos a la pagina de inicio y detalle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,21 +15,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar />
-      <div className="bg-white">
-        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Productos:</h2>
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {data?.map(product =>
-              <ProductCard key={product.id}
-                id={product.id}
-                name={product.name}
-                slug={product.slug}
-                description={product.description}
-                inventory={product.inventory}
-                price={Number(product.price)}
-              />
-            ) ?? <span>Cargando...</span>}
-          </div>
+      <div className="mx-auto max-w-2xl sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 p-3">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Productos:</h2>
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {data?.map(product =>
+            <ProductCard key={product.id}
+              id={product.id}
+              name={product.name}
+              slug={product.slug}
+              description={product.description}
+              inventory={product.inventory}
+              price={Number(product.price)}
+            />
+          ) ?? <span>Cargando...</span>}
         </div>
       </div>
     </>
--- src/pages/product-detail/[id]/index.tsx
+++ src/pages/product-detail/[id]/index.tsx
@@ -29,32 +29,28 @@ const ProductDetail: NextPage = () => {
   return (
     <>
       <NavBar />
-      <div className="bg-white">
-        <div className="pt-6">
-          <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
-            <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-              <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{name}</h1>
+      <div className="mx-auto max-w-2xl px-4 pt-5 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
+        <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+          <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{name}</h1>
+        </div>
+        <div className="mt-4 lg:row-span-3 lg:mt-0">
+          <p className="text-3xl tracking-tight text-gray-900">{formatPrice(price)}</p>
+          <p className="text-sm font-mediud text-gray-900 mt-2">Inventario: {inventory}</p>
+        </div>
+        <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
+          <div>
+            <div className="space-y-6">
+              <p className="text-base text-gray-900">{slug}</p>
             </div>
-            <div className="mt-4 lg:row-span-3 lg:mt-0">
-              <p className="text-3xl tracking-tight text-gray-900">{formatPrice(price)}</p>
-              <p className="text-sm font-mediud text-gray-900 mt-2">Inventario: {inventory}</p>
+          </div>
+          <div className="mt-10">
+            <h2 className="text-sm font-medium text-gray-900">Descripción</h2>
+            <div className="mt-4 space-y-6">
+              <p className="text-sm text-gray-600">{description}</p>
             </div>
-            <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
-              <div>
-                <div className="space-y-6">
-                  <p className="text-base text-gray-900">{slug}</p>
-                </div>
-              </div>
-              <div className="mt-10">
-                <h2 className="text-sm font-medium text-gray-900">Descripción</h2>
-                <div className="mt-4 space-y-6">
-                  <p className="text-sm text-gray-600">{description}</p>
-                </div>
-                <div className="flex items-center justify-end mt-5">
-                  <EditProductButton id={id} />
-                  <DeleteProductButton id={id} />
-                </div>
-              </div>
+            <div className="flex items-center justify-end gap-2 mt-2">
+              <EditProductButton id={id} />
+              <DeleteProductButton id={id} />
             </div>
           </div>
         </div>
